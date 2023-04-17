e841e83fdd805ddbd2fb6239647bb2fd374ece62
binhe924@163.com <binhe924@163.com>
2023-04-17 14:20:06 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,22 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: () => import('../views/secondary.vue')
+	}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history', // hash,注意，history部署时需要配置，不然会404
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
---
+++ src/views/secondary.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="secondary">
+		<h1>This is an secondary page</h1>
+	</div>
+</template>
